57b67fb70e80c46661b8e666e27a46f49919622f
Amar Bajric <amarbajric@Amars-MBP.home>
2024-02-17 23:55:09 +0100

    fix: Appending of fzf line in vimrc caused entire overwrite

--- vim-install.sh
+++ vim-install.sh
@@ -16,22 +16,11 @@ else
     home=$HOME
 fi
 
-
 # Install/Setup Pathogen
 mkdir -p $home/.vim/autoload $home/.vim/bundle
 curl -LSso $home/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# Copy the included .vimrc to the $HOME directory
-vimrcFile=$home/.vimrc
-if test -f "$vimrcFile"; then
-    echo ".vimrc file already exists! Backing up..."
-    cp $home/.vimrc $home/.vimrc.backup.old
-    echo "Existing .vimrc file was backed up under $home/.vimrc.backup.old"
-fi
-cp ./vimrc.txt $home/.vimrc
-
-# Start installing plugins
-
+# Install plugins
 # lightline.vim
 git clone https://github.com/itchyny/lightline.vim $home/.vim/bundle/lightline.vim
 
@@ -53,11 +42,11 @@ git clone https://github.com/airblade/vim-gitgutter.git $home/.vim/bundle/vim-gi
 # fzf
 if [[ $machine == "macOS" ]]; then
     brew install fzf
-    echo "set rtp+=/usr/local/opt/fzf" >> $home/.vimrc
+    fzfCmd="set rtp+=/usr/local/opt/fzf"
 else
     git clone --depth 1 https://github.com/junegunn/fzf.git $home/.fzf
     $home/.fzf/install --bin
-    echo "set rtp+=~/.fzf" >> $home/.vimrc
+    fzfCmd="set rtp+=~/.fzf"
 fi
 
 # animate.vim
@@ -65,5 +54,16 @@ git clone https://github.com/camspiers/animate.vim $home/.vim/bundle/vim-animate
 
 # lens.vim
 git clone https://github.com/camspiers/lens.vim $home/.vim/bundle/vim-lens
+
+# Copy the included .vimrc to the $HOME directory
+vimrcFile=$home/.vimrc
+if test -f "$vimrcFile"; then
+    echo ".vimrc file already exists! Backing up..."
+    cp $home/.vimrc $home/.vimrc.backup.old
+    echo "Existing .vimrc file was backed up under $home/.vimrc.backup.old"
+fi
+
+cp ./vimrc.txt $home/.vimrc
+echo "$fzfCmd" >> $home/.vimrc
 
 echo "Installation complete!"
